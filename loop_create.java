49f71660d914b263f8288a07c99f0b13ed8be52a
Matthew <Bean6754@users.noreply.github.com>
2017-02-16 09:59:07 +0000

    Create loop_create.java

---
+++ loop_create.java
@@ -0,0 +1,49 @@
+import java.lang.Math;
+import java.io.File;
+import java.io.IOException;
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    //Class1 loop_message = new Class1("Loop!!\n");
+    int i;
+    
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+    for (i = 1; i < 6; i++) {
+        
+        
+        try {
+	     File file = new File(i + ".txt");
+	     /*If file gets created then the createNewFile() 
+	      * method would return true or if the file is 
+	      * already present it would return false
+	      */
+             boolean fvar = file.createNewFile();
+	     if (fvar){
+	          System.out.println("File has been created successfully");
+	     }
+	     else{
+	          System.out.println("File already present at the specified location");
+	     }
+    	} catch (IOException e) {
+    		System.out.println("Exception Occurred:");
+	        e.printStackTrace();
+	  }
+        
+        
+        
+      System.out.print("Created file: ");
+      System.out.println(i + ".");
+    }
+  }
+}
+/*
+public class Class1
+{
+ public Class1()
+ {
+   
+ }
+} */
